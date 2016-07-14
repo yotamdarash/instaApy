5250b1c8cb8342adb69a700553b909b74c6721a1
yotamdarash <yotam.darash@gmail.com>
2016-07-14 14:23:49 -0700

    add test_instagram.setUpUser empty test

--- test_instagram.py
+++ test_instagram.py
@@ -1,5 +1,13 @@
 from unittest import TestCase
+from client import Instagram
 
 
 class TestInstagram(TestCase):
-    pass
+    def setUp(self):
+        api = Instagram(client_id='b3e2bf83b14747e89cb526adf934563c',
+                        client_secret='7855bdc2ae4341468784204895aaa0a6',
+                        client_ips='189464193',
+                        access_token='189464193.b3e2bf8.1a1a1f0696a84017b1c4bac1443f892e',
+                        redirect_uri=None)
+    def setUpUser(self):
+        pass
