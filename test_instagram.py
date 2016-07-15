f91fe0209c72a0ecb2f0fc4cc6bde8e8477cbf6e
yotamdarash <yotam.darash@gmail.com>
2016-07-14 20:52:46 -0700

    add failling tests

--- test_instagram.py
+++ test_instagram.py
@@ -1,9 +1,12 @@
 from unittest import TestCase
 
-from client import Instagram
 from User import User
+from client import Instagram
+
 
 class TestInstagram(TestCase):
+    user = None
+
     def setUp(self):
         api = Instagram(client_id='b3e2bf83b14747e89cb526adf934563c',
                         client_secret='7855bdc2ae4341468784204895aaa0a6',
@@ -12,11 +15,16 @@ class TestInstagram(TestCase):
                         redirect_uri=None)
 
     def setup_user(self):
-        user =
+        self.user = User(client_id='b3e2bf83b14747e89cb526adf934563c',
+                         client_secret='7855bdc2ae4341468784204895aaa0a6',
+                         client_ips='189464193',
+                         access_token='189464193.b3e2bf8.1a1a1f0696a84017b1c4bac1443f892e',
+                         redirect_uri=None)
 
     def test_user_self(self):
-        self.fail()
-
+        self.setup_user()
+        response = self.user.self()
+        self.assertEqual(response.code,200)
     def test_user_self_recent_media(self):
         self.fail()
 
