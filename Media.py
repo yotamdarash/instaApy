039c13733afb16c0b8812b71ebf69f111fb7e319
yotamdarash <yotam.darash@gmail.com>
2016-08-04 13:06:22 -0700

    all 3 media endpoint passing besic response code tests

---
+++ Media.py
@@ -0,0 +1,33 @@
+from client import Instagram
+
+
+class Media(Instagram):
+    endpoint_base = "/media"
+
+    def __init__(self, **kwargs):
+        super(Media, self).__init__(**kwargs)
+
+    def media_id(self, media_id):
+        oauth_params = self.ACCESS_TOKEN_ONLY
+        params = {}
+        endpoint = self.endpoint_base + "/" + str(media_id)
+        response = self.get_request(endpoint, oauth_params, params)
+        return response
+
+    def media_shortcode(self, shortcode):
+        oauth_params = self.ACCESS_TOKEN_ONLY
+        params = {}
+        endpoint = self.endpoint_base + "/shortcode/" + str(shortcode)
+        response = self.get_request(endpoint, oauth_params, params)
+        return response
+
+    def media_search(self, latitude=None, longitude=None, distance=1000):
+        oauth_params = self.ACCESS_TOKEN_ONLY
+        params = {}
+        if latitude and longitude:
+            params.update({"lat": latitude, "lng": longitude})
+        if distance != 1000:
+            params.update({"distance": distance})
+        endpoint = self.endpoint_base + "/search"
+        response = self.get_request(endpoint, oauth_params, params)
+        return response
