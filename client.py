d9b40def5134d7b02567a6d8e4dd050b1eab7a9f
yotamdarash <yotam.darash@gmail.com>
2016-08-11 14:34:48 -0700

    refactoring : all methods made 'private'

--- client.py
+++ client.py
@@ -13,36 +13,36 @@ class Client(OAuth2API):
     def __init__(self, **kwargs):
         super(Client, self).__init__(**kwargs)
 
-    def build_path(self, endpoint):
+    def _build_path(self, endpoint): #TODO: make private
         return self.host + self.base_path + endpoint
 
-    def build_params(self, params):
+    def _build_params(self, params):
         return params
 
-    def build_oauth_params(self, params):
+    def _build_oauth_params(self, params):
         if params == self.ACCESS_TOKEN_ONLY:
             return {"access_token": self.access_token}
         else:
             raise NotImplementedError("ouath params {} not implemented".format(params))
 
-    def parse_request(self, endpoint, accepted_oauth_params, accepted_params):
-        path = self.build_path(endpoint)
-        params = self.build_params(accepted_params)
-        params.update(self.build_oauth_params(accepted_oauth_params))
+    def _parse_request(self, endpoint, accepted_oauth_params, accepted_params):
+        path = self._build_path(endpoint)
+        params = self._build_params(accepted_params)
+        params.update(self._build_oauth_params(accepted_oauth_params))
 
         return path, params
 
-    def get_request(self, endpoint, accepted_oauth_params, accepted_params):
-        path, params = self.parse_request(endpoint, accepted_oauth_params, accepted_params)
+    def _get_request(self, endpoint, accepted_oauth_params, accepted_params):
+        path, params = self._parse_request(endpoint, accepted_oauth_params, accepted_params)
         r = requests.get(path, params=params)
         return r
 
-    def post_request(self, endpoint, accepted_oauth_params, accepted_params):
-        path, params = self.parse_request(endpoint, accepted_oauth_params, accepted_params)
+    def _post_request(self, endpoint, accepted_oauth_params, accepted_params):
+        path, params = self._parse_request(endpoint, accepted_oauth_params, accepted_params)
         r = requests.post(path, data=params)
         return r
 
-    def delete_request(self, endpoint, accepted_oauth_params, accepted_params):
-        path, params = self.parse_request(endpoint, accepted_oauth_params, accepted_params)
+    def _delete_request(self, endpoint, accepted_oauth_params, accepted_params):
+        path, params = self._parse_request(endpoint, accepted_oauth_params, accepted_params)
         r = requests.delete(path, params=params)
         return r
