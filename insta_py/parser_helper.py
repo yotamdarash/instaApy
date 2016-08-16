2b672c1d8d44651b8d003c0f97a79386b466b50b
yotamdarash <yotam.darash@gmail.com>
2016-08-15 21:42:06 -0700

    debuging location model

--- insta_py/parser_helper.py
+++ insta_py/parser_helper.py
@@ -1,6 +1,5 @@
 import json
-
-
+import models
 def get_envelope(response):
     return json.loads(response.text)
 
@@ -19,3 +18,38 @@ def get_pagination(response):
         return parsed_response["pagination"]
     else:
         return None
+
+def _build_pagination_info(content_obj):
+    """Extract pagination information in the desired format."""
+    pagination = content_obj.get('pagination', {})
+    # if self.pagination_format == 'next_url':
+    #     return pagination.get('next_url')
+    # if self.pagination_format == 'dict':
+    return pagination
+    # raise Exception('Invalid value for pagination_format: %s' % self.pagination_format)
+
+def parse_media(response):
+    return models.Media(get_data(response)), _build_pagination_info(get_envelope(response))
+
+def parse_user(response):
+    return models.User(get_data(response)), _build_pagination_info(get_envelope(response))
+
+def parse_tag(response):
+    return models.Tag(get_data(response)), _build_pagination_info(get_envelope(response))
+def parse_comment(response):
+    api_response = []
+    for entry in get_data(response):
+        obj = models.Tag(entry)
+        api_response.append(obj)
+    return api_response, _build_pagination_info(get_envelope(response))
+def parse_relationship(response):
+    return models.Relationship(get_data(response)), _build_pagination_info(get_envelope(response))
+def parse_shortcode(response):
+    return models.MediaShortcode(get_data(response)), _build_pagination_info(get_envelope(response))
+def parse_location(response):
+    api_response = []
+    for i,entry in enumerate(get_data(response)):
+        obj = models.Location(i,entry)
+        api_response.append(obj)
+    return api_response, _build_pagination_info(get_envelope(response))
+    # return models.Location(get_data(response)), _build_pagination_info(get_envelope(response))
--- insta_py/tests/test_parser_helper.py
+++ insta_py/tests/test_parser_helper.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
 
 from app_info import AppInfo
-from insta_py import InstagramAPI, get_meta, get_pagination, get_data, get_envelope
+from insta_py import InstagramAPI, get_meta, get_pagination, get_data, get_envelope, \
+    parse_media, parse_user, parse_tag, parse_comment, parse_relationship, parse_location
 
 
 class TestGet_envelope(TestCase):
@@ -27,3 +28,51 @@ class TestGet_envelope(TestCase):
     def test_get_pagination(self):
         pagination = get_pagination(self.test_response)
         self.assertIsNotNone(pagination)
+
+    def test_parse_media(self):
+        response = self.api.users.self_recent_media(count=2)
+        media, pagination = parse_media(response)
+        try:
+            self.assertIsNotNone(media.id)
+        except TypeError:
+            self.fail()
+
+    def test_parse_user(self):
+        response = self.api.users.self()
+        user, pagination = parse_user(response)
+        try:
+            self.assertIsNotNone(user.id.username)
+        except TypeError:
+            self.fail()
+
+    def test_parse_tag(self):
+        response = self.api.tags.tag_info("paleo")
+        tag, pagination = parse_tag(response)
+        try:
+            self.assertIsNotNone(tag.name.media_count)
+        except TypeError:
+            self.fail()
+
+    def test_parse_comment(self):
+        response = self.api.comments.get_comments(self.MEDIA_ID)
+        comment, pagination = parse_comment(response)
+        try:
+            self.assertIsNotNone(comment[0].name)
+        except TypeError:
+            self.fail()
+
+    def test_parse_relationship(self):
+        response = self.api.relationships.follows()
+        relationship, pagination = parse_relationship(response)
+        try:
+            self.assertIsNotNone(relationship.incoming_status)
+        except TypeError:
+            self.fail()
+
+    def test_parse_location(self):
+        response = self.api.locations.locations_info(1)
+        location, pagination = parse_location(response)
+        try:
+            self.assertIsNotNone(location.incoming_status)
+        except TypeError:
+            self.fail()
